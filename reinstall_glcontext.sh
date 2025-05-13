07374d3266f22d4f8877b5e258de2fe72ef81596
eundersander <eundersander@meta.com>
2025-05-13 14:20:38 -0500

    updates

--- reinstall_glcontext.sh
+++ reinstall_glcontext.sh
@@ -4,5 +4,6 @@ export CPATH=$PWD/nvidia-gl
 export CFLAGS="-I$PWD/nvidia-gl"
 export LDFLAGS="-L$PWD/nvidia-gl"
 
+rm -rf ~/.cache/pip/wheels
 pip uninstall -y glcontext
 pip install -vvv --no-binary glcontext glcontext 2>&1 | tee build.log
