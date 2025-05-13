4ceb7461e30e6ea2d4573006ba76df67dc3939b5
eundersander <eundersander@meta.com>
2025-05-13 15:01:39 -0500

    updates

--- set_env_for_egl.sh
+++ set_env_for_egl.sh
@@ -1,3 +1,6 @@
+
+export DRIVER_LIB_DIR=/lib/x86_64-linux-gnu
+
 export __EGL_VENDOR_LIBRARY_FILENAMES=$PWD/nvidia-gl/nvidia.json
-export LD_LIBRARY_PATH=$PWD/nvidia-gl:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$PWD/nvidia-gl:$DRIVER_LIB_DIR:$LD_LIBRARY_PATH
 export PYOPENGL_PLATFORM=egl
