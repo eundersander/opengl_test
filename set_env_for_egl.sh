cf015b036a32c7a86a6236657281b2f856dda9a2
eundersander <eundersander@meta.com>
2025-05-14 00:30:55 -0500

    updates

--- set_env_for_egl.sh
+++ set_env_for_egl.sh
@@ -5,8 +5,10 @@ unset LD_DEBUG
 
 export __EGL_VENDOR_LIBRARY_FILENAMES=$PWD/nvidia-gl/nvidia.json
 
+# note we clear all other stuff in LD_LIBRARY_PATH
+
 # do we want to consider so files in /lib/x86_64-linux-gnu?
-export LD_LIBRARY_PATH=$PWD/nvidia-gl:/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$PWD/nvidia-gl
 
 # export LD_LIBRARY_PATH=$PWD/nvidia-gl:$LD_LIBRARY_PATH
 
