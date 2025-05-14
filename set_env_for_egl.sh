8a0218c932c53698157300f9361c8dc441f0431d
eundersander <eundersander@meta.com>
2025-05-13 23:48:26 -0500

    updates

--- set_env_for_egl.sh
+++ set_env_for_egl.sh
@@ -3,10 +3,9 @@
 export __EGL_VENDOR_LIBRARY_FILENAMES=$PWD/nvidia-gl/nvidia.json
 
 # do we want to consider so files in /lib/x86_64-linux-gnu?
-# export DRIVER_LIB_DIR=/lib/x86_64-linux-gnu
-# export LD_LIBRARY_PATH=$PWD/nvidia-gl:$DRIVER_LIB_DIR:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$PWD/nvidia-gl:/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
 
-export LD_LIBRARY_PATH=$PWD/nvidia-gl:$LD_LIBRARY_PATH
+# export LD_LIBRARY_PATH=$PWD/nvidia-gl:$LD_LIBRARY_PATH
 
 export PYOPENGL_PLATFORM=egl
 
